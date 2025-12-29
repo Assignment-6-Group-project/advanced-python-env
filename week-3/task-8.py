944f8d14645e22d13ad436520fc791a852c571ea
dimkulbek-star <dim.kulbek@gmail.com>
2025-12-29 12:44:44 +0500

    tasks

--- week-3/task-8.py
+++ week-3/task-8.py
@@ -15,7 +15,20 @@ for num in range(1, n + 1):
 
 
 #2
+def swap(arr):
+    arr[0], arr[-1] = arr[-1], arr[0]
 
+m = int(input("Enter array length: "))
+A = []
+
+for i in range(m):
+    A.append(int(input()))
+
+print("Original array:", A)
+
+swap(A)
+
+print("Modified array:", A)
 
 
 
